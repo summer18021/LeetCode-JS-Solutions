fce321c650d6dfb3e755ea1720c1957eaf42cba7
summer18021 <summer18021@gmail.com>
2017-03-09 16:52:51 -0800

    Update BalancedBinaryTree.js

--- DFS/BalancedBinaryTree.js
+++ DFS/BalancedBinaryTree.js
@@ -17,16 +17,16 @@ in which the depth of the two subtrees of every node never differ by more than 1
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    if(root===null) return true;
-    return helper(root)!==-1;
+    if (root === null) return true;
+    return helper(root) !== -1;
 };
-var helper = function(node){
-    if(node===null) return 0;
+var helper = function(node) {
+    if (node === null) return 0;
     var lH = helper(node.left);
-    if(lH===-1) return -1;
+    if (lH === -1) return -1;
     var rH = helper(node.right);
-    if(rH===-1) return -1;
+    if (rH === -1) return -1;
     
-    if(lH-rH>1 || rH-lH>1) return -1;
+    if (lH-rH>1 || rH-lH>1) return -1;
     return Math.max(lH, rH) + 1;
 };
