6aec672282dbfc22e3d4e96bcf16551e8b644b3d
summer18021 <summer18021@gmail.com>
2017-03-29 15:15:00 -0700

    226. Invert Binary Tree

---
+++ InvertBinaryTree.js
@@ -0,0 +1,36 @@
+/*
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    //recursive DFS
+    if (root === null) return null;
+    var left = root.left;
+    var right = root.right;
+    root.left = invertTree(right);
+    root.right = invertTree(left);
+    return root;
+};
